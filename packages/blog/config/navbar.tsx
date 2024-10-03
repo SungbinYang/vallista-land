d5efd1952fd26e7d66256a2c9149d11a0de8a026
SungbinYang <ysb05222@naver.com>
2024-10-03 16:14:02 +0900

    프로필 변경

--- packages/blog/config/navbar.tsx
+++ packages/blog/config/navbar.tsx
@@ -6,7 +6,7 @@ import profile from '../static/profile.png'
 export const NavCategory: NavCategoryType = {
   me: {
     name: '제가 궁금하세요?',
-    icon: <Image src={profile} width={55} height={65} />,
+    icon: <Image src={profile} width={55} height={Math.round((960 / 720) * 55)} />,
     link: '/resume'
   },
   home: {
